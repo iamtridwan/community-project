b0b77e34f2713009c4fefcdd0baae7fa2abdba37
Tijani Ridwan <iamtridwan@gmail.com>
2021-12-21 21:50:14 +0100

    updated ng json

--- src/app/core/courses-sevices.service.ts
+++ src/app/core/courses-sevices.service.ts
@@ -12,19 +12,19 @@ export class CoursesSevicesService {
   secret =
     '3dpCNmER5iBZmGZnWh6sBMpp3ANv0i9p9f0QsgQXCYxjkktPSLDKBNFTcWas9gHMWkHOQcnAExfjp9yOzyCjlfTKsDwwu60eGwnxWxGiuIJz31UZutths4tIXJUVc5l7';
 
+
+   
   constructor(private _http: HttpClient) {}
 
   getCourses(url: string): Observable<any> {
     return this._http.get<any>(url, {
-      headers: {
-        method: 'GET',
+      headers: new HttpHeaders({
         Accept: 'application/json, text/plain, */*',
-        Authorization:
-          'Basic eW9LcVZldklWbUlIeGdhdmd0YWFOTGI3cWlwSGdQaU96VVNMNmZHSjoJM2RwQ05tRVI1aUJabUdabldoNnNCTXBwM0FOdjBpOXA5ZjBRc2dRWENZeGpra3RQU0xES0JORlRjV2FzOWdITVdrSE9RY25BRXhmanA5eU96eUNqbGZUS3NEd3d1NjBlR3dueFd4R2l1SUp6MzFVWnV0dGhzNHRJWEpVVmM1bDc=' + this.id + ':' + this.secret,
+        Authorization: `Basic eW9LcVZldklWbUlIeGdhdmd0YWFOTGI3cWlwSGdQaU96VVNMNmZHSjoJM2RwQ05tRVI1aUJabUdabldoNnNCTXBwM0FOdjBpOXA5ZjBRc2dRWENZeGpra3RQU0xES0JORlRjV2FzOWdITVdrSE9RY25BRXhmanA5eU96eUNqbGZUS3NEd3d1NjBlR3dueFd4R2l1SUp6MzFVWnV0dGhzNHRJWEpVVmM1bDc=${this.id}:${this.secret}`,
         'Content-Type': 'application/json;charset=utf-8',
-        mode: 'no-cors'
 
-      },
+        
+      }),
     });
   }
 
