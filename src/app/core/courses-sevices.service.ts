41c372fd51a7e8d30fa38b5b4264fef5d71d1a52
Tijani Ridwan <iamtridwan@gmail.com>
2022-01-20 13:45:21 +0100

    added path

--- src/app/core/courses-sevices.service.ts
+++ src/app/core/courses-sevices.service.ts
@@ -12,24 +12,25 @@ import { Course } from '../models/course';
 export class CoursesSevicesService {
   // learners_url = '../assets/data/learners.json';
   // change this url when you deploy
-  learners_url = 'http://localhost:3000/users';
-  id = 'yoKqVevIVmIHxgavgtaaNLb7qipHgPiOzUSL6fGJ';
-  id2 = 'hdVJebnKXi4wjtfxNDGbcSgr46KIubk4flfFmSed';
-  secret =
-    '3dpCNmER5iBZmGZnWh6sBMpp3ANv0i9p9f0QsgQXCYxjkktPSLDKBNFTcWas9gHMWkHOQcnAExfjp9yOzyCjlfTKsDwwu60eGwnxWxGiuIJz31UZutths4tIXJUVc5l7';
-  secret2 =
-    'i2cK7LeDMPfoAh4WD3ljLKytfhHnCKacjTisMuMWQvTkV0iBQQLWrJHCMvHDir9xJWjVeVH1lZ7pJTnkURSLt4w0NibxKwWaIPXq1wa38F884VHq8ugdy1pKnbaIRwSF';
-  url =
-    'get /api-2.0/courses/?page=2&category=IT%20&%20Software&price=price-paid';
+  // learners_url = 'http://localhost:3000/users';
+  learners_url = 'https://guarded-peak-92292.herokuapp.com/users';
+  // id = 'yoKqVevIVmIHxgavgtaaNLb7qipHgPiOzUSL6fGJ';
+  // id2 = 'hdVJebnKXi4wjtfxNDGbcSgr46KIubk4flfFmSed';
+  // secret =
+  //   '3dpCNmER5iBZmGZnWh6sBMpp3ANv0i9p9f0QsgQXCYxjkktPSLDKBNFTcWas9gHMWkHOQcnAExfjp9yOzyCjlfTKsDwwu60eGwnxWxGiuIJz31UZutths4tIXJUVc5l7';
+  // secret2 =
+  //   'i2cK7LeDMPfoAh4WD3ljLKytfhHnCKacjTisMuMWQvTkV0iBQQLWrJHCMvHDir9xJWjVeVH1lZ7pJTnkURSLt4w0NibxKwWaIPXq1wa38F884VHq8ugdy1pKnbaIRwSF';
+  // url =
+  //   'get /api-2.0/courses/?page=2&category=IT%20&%20Software&price=price-paid';
 
-  headers =new HttpHeaders ({
+  headers = new HttpHeaders({
     'Content-Type': 'application/json',
-    'Allow-Access-Control-Origin': '*'
+    'Allow-Access-Control-Origin': '*',
   });
   constructor(private _http: HttpClient) {}
 
   getCourses(url: string): Observable<Course[]> {
-    return this._http.get<Course[]>(url,{headers: this.headers});
+    return this._http.get<Course[]>(url, { headers: this.headers });
   }
 
   getRegisteredLearners(): Observable<Learner[]> {
